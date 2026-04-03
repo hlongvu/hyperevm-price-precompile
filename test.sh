af2c239932c068fdbbc5f2b365486fb5c54f49c7
Long Vu <hlongvu@gmail.com>
2026-04-03 15:54:02 +0700

    add docs

--- test.sh
+++ test.sh
@@ -17,3 +17,17 @@ cast call 0x0000000000000000000000000000000000000807 0x0000000000000000000000000
 
 forge build
 forge script script/DeployPriceOracleReader.s.sol:PriceOracleReaderScript --rpc-url $TESTNET_RPC_URL --account fcb8 --broadcast --gas-limit 25000000
+
+
+##### 998
+✅  [Success] Hash: 0xc06fbc3d62396563c67dc4f70c930a2f9ca39800be42206e5b76dd16e143c256
+Contract Address: 0x6F3E9A9A86a136bEe32C32F6ac6442cC90a41BAF
+Block: 49884492
+Paid: 0.000429131504291315 ETH (4291315 gas * 0.100000001 gwei)
+
+
+### Get price
+cast call 0x6F3E9A9A86a136bEe32C32F6ac6442cC90a41BAF "updatePrice(uint32)(uint256)" 4 --rpc-url $TESTNET_RPC_URL
+
+### sign l1 action to use big block
+https://github.com/hyperliquid-dex/hyperliquid-python-sdk/blob/master/examples/basic_evm_use_big_blocks.py
